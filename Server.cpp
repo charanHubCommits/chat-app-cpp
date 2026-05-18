41c39188d0b227c2f8159c300ef79b78fed0c352
charanHubCommits <charanmandakuriti@gmail.com>
2026-05-18 19:55:32 +0530

    updated class structure and handled blocking user registration

--- Server.cpp
+++ Server.cpp
@@ -11,17 +11,14 @@
 #include<unordered_map>
 
 #define PORT "4000"
-#define MAX_CLIENTS 20
+#define MAX_CLIENTS 1024
 
-class Server {
-	private:
-		int serverFd = -1,bindStatus = 0;
-		struct addrinfo *res = nullptr;
-		std::vector<struct pollfd> pfds;
-		std::vector<int> clientFds;
-		std::unordered_map<std::string ,int> clientMp;
+class TcpSocket {
 	public:
-		Server() {
+		int sockFd = -1,bindStatus = 0;
+		struct addrinfo *res = nullptr;
+
+		TcpSocket() {
 			struct addrinfo hints;
 			memset(&hints,0,sizeof hints);
 			hints.ai_family = AF_INET;
@@ -35,30 +32,48 @@ class Server {
 				return;
 			}
 
-			serverFd = socket(res->ai_family,res->ai_socktype,res->ai_protocol);
+			sockFd = socket(res->ai_family,res->ai_socktype,res->ai_protocol);
 
-			if(serverFd<0) {
+			if(sockFd<0) {
 				std::cout << "Socket creation failed!" << std::endl;
 				freeaddrinfo(res);
 				return;
 
 			}
 			int opt = 1;
-			setsockopt(serverFd,SOL_SOCKET,SO_REUSEADDR,&opt,sizeof(opt));
+			setsockopt(sockFd,SOL_SOCKET,SO_REUSEADDR,&opt,sizeof(opt));
 			
-			bindStatus = bind(serverFd,res->ai_addr,res->ai_addrlen);
+			bindStatus = bind(sockFd,res->ai_addr,res->ai_addrlen);
 
 			if(bindStatus<0) {
 				std::cout << "Bind Failed!" << std::endl;
-				close(serverFd);
+				close(sockFd);
 				freeaddrinfo(res);
 				res = nullptr;
-				serverFd = -1;
+				sockFd = -1;
 				return;
 			}
-			
+		}
+
+		~TcpSocket() {
+			freeaddrinfo(res);
+			if(sockFd >=0) {
+				close(sockFd);
+			}
+		}
+};
+
+class Server {
+	private:
+		TcpSocket sock;
+		std::vector<struct pollfd> pfds;
+		std::vector<int> clientFds;
+		std::unordered_map<std::string ,int> clientMp;
+		std::unordered_map<int,bool> isRegistered;
+	public:
+		Server() {	
 			pfds.resize(MAX_CLIENTS+1);
-			pfds[0].fd=serverFd;
+			pfds[0].fd=sock.sockFd;
 			pfds[0].events = POLLIN;
 
 			for(int i = 1;i<pfds.size();i++) {
@@ -85,12 +100,12 @@ class Server {
 		}
 
 		void run() {
-			if(serverFd < 0 || bindStatus < 0) {
+			if(sock.sockFd < 0 || sock.bindStatus < 0) {
 				std::cout << "Server not available!" << std::endl;
 				return;
 			}
 
-			listen(serverFd,5);
+			listen(sock.sockFd,5);
 			std::cout << "Server running at port: " << PORT << "Max clients: " << MAX_CLIENTS<< std::endl;
 
 			while(true) {
@@ -113,81 +128,72 @@ class Server {
 					socklen_t clientAddrLen = sizeof(clientAddr);
 
 
-					int clientFd = accept(serverFd,(sockaddr*)&clientAddr,&clientAddrLen);
+					int clientFd = accept(sock.sockFd,(sockaddr*)&clientAddr,&clientAddrLen);
 
 					if(clientFd < 0) {
 						continue;
 					}
 
+					isRegistered[clientFd] = false;
+
 					for(int i=0;i<pfds.size();i++) {
 						if(pfds[i].fd == -1) {
-							pfds[i].fd = clientFd;
-							pfds[i].events = POLLIN;
-							pfds[i].revents = 0;
+						pfds[i].fd = clientFd;
+						pfds[i].events = POLLIN;
+						pfds[i].revents = 0;
 
-							std::cout<<"New client connected" << std::endl;
-							char username[20];
-							ssize_t n = recv(clientFd,username,sizeof(username)-1,0);
-							if(n<=0) {
-								close(clientFd);
-								continue;
-							}
-							username[n] = '\0';
-							std::string name(username);
-							while(clientMp.find(name)!=clientMp.end()) {
-								std::string warn = "Username already exists!\nEnter another username: ";
-								send(clientFd,warn.c_str(),warn.size(),0);
-								memset(username,0,sizeof(username)-1);
-
-							 	n = recv(clientFd,username,sizeof(username)-1,0);
-								if(n<=0) {
-									close(clientFd);
-									continue;
-								}
-								name = std::string(username);
-							}
-
-
-							clientMp[name] = clientFd;
-							std::cout << "New user added " << name <<" " << clientFd << std::endl;
-							break;
-						}
+						std::cout<<"New client connected" << std::endl;	
+						break;
 					}
-
-					if(pfds[num_fds-1].fd !=-1 && num_fds > MAX_CLIENTS) {
-						close(clientFd);
-					}
-
-					if(--pollCount <=0) continue;
 				}
 
-				for(int i=1;i<num_fds;i++) {
-					if(pfds[i].fd==-1) continue;
+				if(pfds[num_fds-1].fd !=-1 && num_fds > MAX_CLIENTS) {
+					close(clientFd);
+				}
 
-					if(pfds[i].revents & (POLLIN | POLLERR | POLLHUP)) {
-						char buffer[2048];
+				if(--pollCount <=0) continue;
+			}
 
-						ssize_t bytesReceived = recv(pfds[i].fd,buffer,sizeof(buffer)-1,0);
+			for(int i=1;i<num_fds;i++) {
+				if(pfds[i].fd==-1) continue;
 
-						if(bytesReceived > 0) {
-							buffer[bytesReceived] = '\0';
+				if(pfds[i].revents & (POLLIN | POLLERR | POLLHUP)) {
+					char buffer[2048];
 
-							std::string packet(buffer);
-							std::cout << "User sent " << packet << std::endl;
+					ssize_t bytesReceived = recv(pfds[i].fd,buffer,sizeof(buffer)-1,0);
 
+					if(bytesReceived > 0) {
+						buffer[bytesReceived] = '\0';
+
+						std::string packet(buffer);
+						std::cout << "User sent " << packet << std::endl;
+
+						if(!isRegistered[pfds[i].fd]) {
+							if(clientMp.find(packet) != clientMp.end()) {
+								std::string err = "Username already exists!";
+								send(pfds[i].fd,err.c_str(),err.size(),0);
+							}
+							else {
+								clientMp[packet] = pfds[i].fd;
+								isRegistered[pfds[i].fd] = true;
+								std::string ack = "Registerd successfully!";
+								send(pfds[i].fd,ack.c_str(),ack.size(),0);
+							}
+						}
+						else {
 							std::array<std::string, 4> packetDetails;
 							getPacketDetails(packet,packetDetails);
 
 							/* PacketDetails
-							 * index 0 - sender
-							 * index 1 - receiver
-							 * index 2 - message
-							 * index 3 - timeStamp */
+							* index 0 - sender
+						 	* index 1 - receiver
+						 	* index 2 - message
+						 	* index 3 - timeStamp */
 
 							std::string receiver = packetDetails[1];
-	
+
 							if(clientMp.find(receiver) != clientMp.end()) {
-								std::string finalMsg = packetDetails[0]+": "+packetDetails[2]+" "+packetDetails[3];
+								std::string finalMsg = packetDetails[0]+": "+packetDetails[2]+" |"+packetDetails[3];
 								send(clientMp[receiver],finalMsg.c_str(),finalMsg.size(),0);
 								std::string ack = "msg sent";
 								send(pfds[i].fd,ack.c_str(),ack.size(),0);
@@ -196,37 +202,36 @@ class Server {
 								std::string err = "user not found";
 								send(pfds[i].fd,err.c_str(),err.size(),0);
 							}
-						}else if(bytesReceived <= 0) {
-							close(pfds[i].fd);
-							for(auto it = clientMp.begin(); it != clientMp.end(); ) {
-    								if(it->second == pfds[i].fd) {
-        								it = clientMp.erase(it);
-    								} else {
-        								++it;
-    								}
-							}
 
-							pfds[i].fd = -1;
-
-							continue;
 						}
+	
+					}else if(bytesReceived <= 0) {
+						close(pfds[i].fd);
+						for(auto it = clientMp.begin(); it != clientMp.end(); ) {
+    							if(it->second == pfds[i].fd) {
+        							it = clientMp.erase(it);
+    							} else {
+								++it;
+							}
+						}
+
+						pfds[i].fd = -1;
+							
+						continue;
 					}
 				}
 			}
-
+		}
+	
 		}
 
 		~Server() {
-			freeaddrinfo(res);
-			if(serverFd >=0) {
-				close(serverFd);
-			}
-
 			for(int i=1;i<pfds.size();i++) {
 				if(pfds[i].fd!=-1) {
 					close(pfds[i].fd);
 				}
 			}
+			std::cout << "Session Ended" << std::endl;
 		}
 };
 
