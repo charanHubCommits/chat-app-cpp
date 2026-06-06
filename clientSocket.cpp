9a90ae2c6db6182c3ef897954ad4ec74f8583d13
charanHubCommits <charanmandakuriti@gmail.com>
2026-06-06 15:24:19 +0530

    fix memory bug in chat() function

--- clientSocket.cpp
+++ clientSocket.cpp
@@ -1,181 +1,175 @@
-#include<iostream>
-#include<sys/socket.h>
-#include<netinet/in.h>
-#include<sys/types.h>
-#include<string>
-#include<cstring>
-#include<unistd.h>
-#include<netdb.h>
-#include<thread>
-#include<chrono>
+#include <chrono>
+#include <cstring>
+#include <iostream>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <string>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <thread>
+#include <unistd.h>
 
 class TcpSocket {
-	public:
-		int sockFd;
-		struct addrinfo *res;
+public:
+  int sockFd;
+  struct addrinfo *res;
 
-		TcpSocket():sockFd(-1),res(nullptr) {
-			/*struct addrinfo hints;
+  TcpSocket() : sockFd(-1), res(nullptr) {
+    /*struct addrinfo hints;
 
-			memset(&hints,0,sizeof hints);
-			hints.ai_family = AF_INET;
-			hints.ai_socktype = SOCK_STREAM;
+    memset(&hints,0,sizeof hints);
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
 
-			int status = getaddrinfo(ip,PORT,&hints,&res);
+    int status = getaddrinfo(ip,PORT,&hints,&res);
 
-			if(status!=0) {
-				std::cerr << "Couldn't connect to server!" << std::endl;
+    if(status!=0) {
+            std::cerr << "Couldn't connect to server!" << std::endl;
 
-				return;
-			}
-			
-			sockFd = socket(res->ai_family,res->ai_socktype,res->ai_protocol);
+            return;
+    }
 
-			if(sockFd == -1) {
-				std::cerr << "Failed to create socket!" << std::endl;
+    sockFd = socket(res->ai_family,res->ai_socktype,res->ai_protocol);
 
-				return;
-			}*/
-		}
+    if(sockFd == -1) {
+            std::cerr << "Failed to create socket!" << std::endl;
 
-		void setAddrInfo(std::string &ip,std::string &port) {
-			struct addrinfo hints;
+            return;
+    }*/
+  }
 
-			memset(&hints,0,sizeof hints);
-			hints.ai_family = AF_INET;
-			hints.ai_socktype = SOCK_STREAM;
+  void setAddrInfo(std::string &ip, std::string &port) {
+    struct addrinfo hints;
 
-			int status = getaddrinfo(ip.c_str(),port.c_str(),&hints,&res);
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
 
-			if(status!=0) {
-				std::cerr << "Couldn't connect to server!" << std::endl;
+    int status = getaddrinfo(ip.c_str(), port.c_str(), &hints, &res);
 
-				return;
-			}
-			
-			sockFd = socket(res->ai_family,res->ai_socktype,res->ai_protocol);
+    if (status != 0) {
+      std::cerr << "Couldn't connect to server!" << std::endl;
 
-			if(sockFd == -1) {
-				std::cerr << "Failed to create socket!" << std::endl;
+      return;
+    }
 
-				return;
-			}
-		}
+    sockFd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
 
-		~TcpSocket() {
-			if(sockFd!=-1)
-				close(sockFd);
-			if(res!=nullptr)
-				freeaddrinfo(res);
-		}
+    if (sockFd == -1) {
+      std::cerr << "Failed to create socket!" << std::endl;
+
+      return;
+    }
+  }
+
+  ~TcpSocket() {
+    if (sockFd != -1)
+      close(sockFd);
+    if (res != nullptr)
+      freeaddrinfo(res);
+  }
 };
 
 class Client {
-	private:
-		TcpSocket sock;
-		std::string username;
-	public:
-		Client(std::string &ip,std::string &port) {
-			sock.setAddrInfo(ip,port);
-		}
+private:
+  TcpSocket sock;
+  std::string username;
 
-		void sendMsg(std::string &msg){
-			auto time = std::chrono::system_clock::now();
-			auto time_t = std::chrono::system_clock::to_time_t(time);
-			std::string timeStamp = (std::string)ctime(&time_t);
-			timeStamp.pop_back(); //removing \n
-	
-			int receiverIndex = msg.find(" ");
-			std::string receiver = msg.substr(0,receiverIndex);
-			std::string finalMsg = msg.substr(receiverIndex+1);
-					
-			std::string packet = username+"|"+receiver+"|"+finalMsg+"|"+timeStamp;
+public:
+  Client(std::string &ip, std::string &port) { sock.setAddrInfo(ip, port); }
 
-			ssize_t sent = send(sock.sockFd,packet.c_str(),packet.size(),0);
+  void sendMsg(std::string &msg) {
+    auto time = std::chrono::system_clock::now();
+    auto time_t = std::chrono::system_clock::to_time_t(time);
+    std::string timeStamp = (std::string)ctime(&time_t);
+    timeStamp.pop_back(); // removing \n
 
-	    		if(sent <= 0) {
-        			std::cout << "Failed to send\n";
-				return;
-			}
-		}
+    int receiverIndex = msg.find(" ");
+    std::string receiver = msg.substr(0, receiverIndex);
+    std::string finalMsg = msg.substr(receiverIndex + 1);
 
-		void chat() {
-			int isConnected=connect(sock.sockFd,sock.res->ai_addr,sock.res->ai_addrlen);
+    std::string packet =
+        username + "|" + receiver + "|" + finalMsg + "|" + timeStamp;
 
-			if(isConnected < 0) {
-				std::cout << "Failed to connect" << std::endl;
-				freeaddrinfo(sock.res);
-	
-				return;
-			}
+    ssize_t sent = send(sock.sockFd, packet.c_str(), packet.size(), 0);
 
-			std::cout << "Connected successfully\n";
-			std::cout << "Enter username to register\n";
+    if (sent <= 0) {
+      std::cout << "Failed to send\n";
+      return;
+    }
+  }
 
-			std::getline(std::cin,username);
-			send(sock.sockFd,username.c_str(),username.size(),0);
+  void chat() {
+    int isConnected =
+        connect(sock.sockFd, sock.res->ai_addr, sock.res->ai_addrlen);
 
-			std::thread recvThread(&Client::receiveMsg,this);
-			
-			std::cout << "Enter 'end_session' to end this session\nEnter username and msg in format: ";
-			std::cout << "<username> <message> to send a msg" << std::endl;	
-			
-					
-			std::string msg;
+    if (isConnected < 0) {
+      std::cout << "Failed to connect" << std::endl;
 
-			while(true) {
-    				std::getline(std::cin,msg);
+      return;
+    }
 
-	    			if(msg == "end_session") {
-       					 break;
-   				 }
-				if(msg.find(" ")!=std::string::npos) {
-					sendMsg(msg);	
-					continue;
-				}
-			
-				std::cout << "Enter valid message format!" << std::endl;
-    			}
-			shutdown(sock.sockFd,SHUT_RDWR);
-			recvThread.join();
-		}
+    std::cout << "Connected successfully\n";
+    std::cout << "Enter username to register\n";
 
-		void receiveMsg() {
-			char buffer[1024];
+    std::getline(std::cin, username);
+    send(sock.sockFd, username.c_str(), username.size(), 0);
 
-			while(true){
-				ssize_t n = recv(sock.sockFd,buffer,sizeof(buffer),0);
-				if(n<=0) {
-					std::cout << "Disconnected from Server!\n";
-					break;
-				}
-				buffer[n]= '\0';
-				std::cout << buffer << std::endl; 
+    std::thread recvThread(&Client::receiveMsg, this);
 
-			}
-		}
+    std::cout << "Enter 'end_session' to end this session\nEnter username and "
+                 "msg in format: ";
+    std::cout << "<username> <message> to send a msg" << std::endl;
 
-		~Client() {
-			std::cout << "Connection closed!" <<std::endl;
-		}
+    std::string msg;
 
+    while (true) {
+      std::getline(std::cin, msg);
+
+      if (msg == "end_session") {
+        break;
+      }
+      if (msg.find(" ") != std::string::npos) {
+        sendMsg(msg);
+        continue;
+      }
+
+      std::cout << "Enter valid message format!" << std::endl;
+    }
+    shutdown(sock.sockFd, SHUT_RDWR);
+    recvThread.join();
+  }
+
+  void receiveMsg() {
+    char buffer[1024];
+
+    while (true) {
+      ssize_t n = recv(sock.sockFd, buffer, sizeof(buffer), 0);
+      if (n <= 0) {
+        std::cout << "Disconnected from Server!\n";
+        break;
+      }
+      buffer[n] = '\0';
+      std::cout << buffer << std::endl;
+    }
+  }
+
+  ~Client() { std::cout << "Connection closed!" << std::endl; }
 };
 
-int main(int argc,char **argv) {
-	if(argc < 3 ) {
-		std::cout << "enter a ip and port of the server" << std::endl;
+int main(int argc, char **argv) {
+  if (argc < 3) {
+    std::cout << "enter a ip and port of the server" << std::endl;
 
-		return 0;
-	}
+    return 0;
+  }
 
-	std::string ip(argv[1]);
-	std::string port(argv[2]);
+  std::string ip(argv[1]);
+  std::string port(argv[2]);
 
-	Client user(ip,port);
+  Client user(ip, port);
 
-	user.chat();
-			
-	return 0;
+  user.chat();
+
+  return 0;
 }
-
-
